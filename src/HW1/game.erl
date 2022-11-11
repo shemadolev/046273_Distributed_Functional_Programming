c41438aed84524803098eeb23cc632e64be484ad
Dolev Shema <shemadolev@gmail.com>
2022-11-11 22:38:08 +0200

    HW1: game solution

--- src/HW1/game.erl
+++ src/HW1/game.erl
@@ -3,12 +3,21 @@
 %% API
 -export([canWin/1, nextMove/1, explanation/0]).
 
+canWin(1) -> true; % by taking 1 match
+canWin(2) -> true; % by taking 2 matches
+canWin(N) -> % I can win if by one of my moves the opponent can't win
+  not(canWin(N - 1) andalso canWin(N - 2)).
 
-canWin(N) ->
-  erlang:error(not_implemented).
-
+nextMove(1) -> {true, 1};
+nextMove(2) -> {true, 2};
 nextMove(N) ->
-  erlang:error(not_implemented).
+  case canWin(N - 1) of
+    false -> {true, 1}; % Opponent loses if I'll take 1
+    true -> case canWin(N - 2) of
+              false -> {true, 2}; % Opponent loses if I'll take 2
+              true -> false % No matter what I'll do, the opponent can win => I must lose
+            end
+  end.
 
 explanation() ->
-  erlang:error(not_implemented).
+  {"As each function depends on a calculation of 2 steps before, an implementation with tail recursion needs to pass a state of the two previous results."}.
