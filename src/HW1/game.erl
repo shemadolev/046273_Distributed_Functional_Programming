72c9db166c09acace3eff99540ed1e5cb46c514d
Dolev Shema <shemadolev@gmail.com>
2022-11-20 00:21:10 +0200

    nextMove explicitly recursive

--- src/HW1/game.erl
+++ src/HW1/game.erl
@@ -5,18 +5,18 @@
 
 canWin(1) -> true; % by taking 1 match
 canWin(2) -> true; % by taking 2 matches
-canWin(N) -> % I can win if by one of my moves the opponent can't win
+canWin(N) when N > 0 -> % I can win if by one of my moves the opponent can't win
   not(canWin(N - 1) andalso canWin(N - 2)).
 
 nextMove(1) -> {true, 1};
 nextMove(2) -> {true, 2};
-nextMove(N) ->
-  case canWin(N - 1) of
+nextMove(N) when N > 0 ->
+  case nextMove(N - 1) of
     false -> {true, 1}; % Opponent loses if I'll take 1
-    true -> case canWin(N - 2) of
-              false -> {true, 2}; % Opponent loses if I'll take 2
-              true -> false % No matter what I'll do, the opponent can win => I must lose
-            end
+    {true, _} -> case nextMove(N - 2) of
+           false -> {true, 2}; % Opponent loses if I'll take 2
+           {true, _} -> false % No matter what I'll do, the opponent can win => I must lose
+         end
   end.
 
 explanation() ->
