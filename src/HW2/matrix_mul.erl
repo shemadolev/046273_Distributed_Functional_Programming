57f32d6dfac3ad1e6cb562280c240bde1915ed37
Dolev Shema <shemadolev@gmail.com>
2022-12-09 18:01:51 +0200

    separate spawn & send logic

--- src/HW2/matrix_mul.erl
+++ src/HW2/matrix_mul.erl
@@ -4,26 +4,25 @@
 %% API
 -export([multiply/2]).
 
-%% Multiplies 2 matrices
+%% Multiplies Mat1*Mat2 in parallel: calculate each cell in the product using a separate process
 multiply(Mat1, Mat2) ->
   Pid = self(),
   Mat1Rows = tuple_size(Mat1),
   Mat2Cols = tuple_size(element(1, Mat2)),
-  [spawn(fun() -> % Spawn a process for each cell in the product
-    calc_cell(Mat1, Mat2, RowIdx, ColIdx, Pid) end) || RowIdx <- lists:seq(1, Mat1Rows), ColIdx <- lists:seq(1, Mat2Cols)],
+  [proc_utils:spawn_send(fun() -> Result = calc_cell(Mat1, Mat2, RowIdx, ColIdx), {RowIdx, ColIdx, Result} end, Pid)
+    || RowIdx <- lists:seq(1, Mat1Rows), ColIdx <- lists:seq(1, Mat2Cols)],
   collect_cell_results(0, Mat1Rows * Mat2Cols, matrix:getZeroMat(Mat1Rows, Mat2Cols)).
 
 %% Sum up all elements in a list
 list_sum(List) ->
   lists:foldl(fun(X, Acc) -> X + Acc end, 0, List).
 
-%% Calc the value of the cell in the position of [RowIdx,ColIdx] in the product of Mat1*Mat2, and send the result to Pid
-calc_cell(Mat1, Mat2, RowIdx, ColIdx, Pid) ->
+%% Calc the value of the cell in the position of [RowIdx,ColIdx] in the product of Mat1*Mat2
+calc_cell(Mat1, Mat2, RowIdx, ColIdx) ->
   Mat1Row = matrix:getRow(Mat1, RowIdx),
   Mat2Col = matrix:getCol(Mat2, ColIdx),
   MulList = [element(Idx, Mat1Row) * element(Idx, Mat2Col) || Idx <- lists:seq(1, tuple_size(Mat1Row))],
-  Sum = list_sum(MulList),
-  Pid ! {RowIdx, ColIdx, Sum}.
+  list_sum(MulList).
 
 %% Collect all the results of each cell calculation from received messages, and combine them into one matrix.
 collect_cell_results(N, N, Matrix) -> Matrix;
