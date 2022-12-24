2a8523d1627fe148ddbaa832f734afee2315653d
Dolev Shema <shemadolev@gmail.com>
2022-12-24 14:06:56 +0200

    start_supervised_register calls using Module name

--- src/HW2/proc_utils.erl
+++ src/HW2/proc_utils.erl
@@ -2,22 +2,24 @@
 -module(proc_utils).
 
 %% API
--export([spawn_send/2, start_supervised_register/2]).
+-export([spawn_send/2, start_supervised_register/4]).
 
 %% Spawn a new process to run Func(), and send its result back in a message to Pid
 spawn_send(Func, Pid) ->
   spawn(fun() -> Result = Func(), Pid ! Result end).
 
 %% Create a supervisor to run ChildFun(), and register the worker's process under RegName
-start_supervised_register(ChildFun, RegName) ->
-  spawn(fun() -> restarter(ChildFun, RegName) end).
+start_supervised_register(Module, ModFunc, Args, RegName) ->
+  spawn(fun() -> restarter(Module, ModFunc, Args, RegName) end).
 
-restarter(ChildFun, RegName) ->
+restarter(Module, ModFunc, Args, RegName) ->
   process_flag(trap_exit, true),
-  Pid = spawn_link(ChildFun),
+  Pid = spawn_link(Module, ModFunc, Args),
   register(RegName, Pid),
   receive
     {'EXIT', Pid, normal} -> ok;
     {'EXIT', Pid, shutdown} -> ok;
-    {'EXIT', Pid, _} -> restarter(ChildFun, RegName)
+    {'EXIT', Pid, _} ->
+      io:format("server died!"),
+      restarter(Module, ModFunc, Args, RegName)
   end.
