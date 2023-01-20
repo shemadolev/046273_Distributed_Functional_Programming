6d0abd5048a3d8214ec3883d29e73e0b4ca3f5e4
Dolev Shema <shemadolev@gmail.com>
2023-01-20 17:40:50 +0200

    implement fun_server

--- src/HW3/fun_server.erl
+++ src/HW3/fun_server.erl
@@ -3,28 +3,39 @@
 -behaviour(gen_server).
 
 %% API
--export([init/1, handle_call/3, handle_cast/2, get_current_functions_count/0, calc_fun/3, start_link/1]).
+-export([handle_call/3, handle_cast/2, get_current_functions_count/1, calc_fun/4, start_link/1, init/1]).
 
 %% ------ Interface Routines -------
 
 %% Get the number of currently running functions on this server
-get_current_functions_count() ->
-  gen_server:call(fun_server, fun_count).
+get_current_functions_count(ServerName) ->
+  gen_server:call(ServerName, fun_count).
 
 %% Start a new function on this server, and send its return value in a message to ClientPid: {MsgRef, ReturnValue}
-calc_fun(ClientPid, ClientFun, MsgRef) ->
-  gen_server:cast(fun_server, {calc_fun, ClientPid, ClientFun, MsgRef}).
+calc_fun(ServerName, ClientPid, ClientFun, MsgRef) ->
+  gen_server:cast(ServerName, {calc_fun, ServerName, ClientPid, ClientFun, MsgRef}).
 
 %% ------ gen_server Callback Routines -------
 
+% Starts the server and registers it (locally) under the name ServerName
 start_link(ServerName) ->
-  gen_server:start_link({local, ?MODULE}, ?MODULE, [ServerName]).
+  gen_server:start_link({local, ServerName}, ?MODULE, [], []).
 
-init(_ServerName) ->
-  erlang:error(not_implemented).
+% State is the number of currently running functions
+init([]) ->
+  {ok, 0}.
 
-handle_call(_Request, _From, _State) ->
-  erlang:error(not_implemented).
+handle_call(fun_count, _From, State) ->
+  {reply, State, State}.
 
-handle_cast(_Request, _State) ->
-  erlang:error(not_implemented).
+handle_cast({calc_fun, ServerName, ClientPid, ClientFun, MsgRef}, State) ->
+  spawn_link( %todo spawn or spawn_link?
+    fun() ->
+      Res = catch ClientFun(),
+      gen_server:cast(ServerName, fun_finished), % signal the server the function has finished
+      ClientPid ! {MsgRef, Res}
+    end),
+  {noreply, State + 1};
+
+handle_cast(fun_finished, State) ->
+  {noreply, State - 1}.
