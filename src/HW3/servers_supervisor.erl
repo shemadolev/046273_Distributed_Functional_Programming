a4e4a7f29b16d2c3cc82371b29377da3acce6d52
Adi Levy <adi.levy@campus.technion.ac.il>
2023-01-20 17:20:15 +0200

    init supervisor

--- src/HW3/servers_supervisor.erl
+++ src/HW3/servers_supervisor.erl
@@ -9,4 +9,11 @@ start_link() ->
   supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
-  erlang:error(not_implemented).
+  Server1 = {server1,{fun_server, start_link, [server1]},
+  permanent, brutal_kill, worker, [fun_server]},
+  Server2 = {server2,{fun_server, start_link, [server2]},
+  permanent, brutal_kill, worker, [fun_server]},
+  Server3 = {server3,{fun_server, start_link, [server3]},
+  permanent, brutal_kill, worker, [fun_server]},
+  {ok,{{one_for_one,1,1}, [Server1, Server2, Server3]}}.
+
