90bfc6abe4cd9c639760d36768d863f2c38c98e2
Dolev Shema <shemadolev@gmail.com>
2023-01-26 20:51:17 +0200

    Add kill test

--- src/HW3/loadBalance_tests.erl
+++ src/HW3/loadBalance_tests.erl
@@ -3,7 +3,7 @@
 -include_lib("eunit/include/eunit.hrl").
 
 simple_test() ->
-  loadBalance:startServer(),
+  loadBalance:startServers(),
   loadBalance:stopServers().
 
 func_to_calc(ExpectedRes, Time) ->
@@ -12,7 +12,7 @@ func_to_calc(ExpectedRes, Time) ->
 
 
 simple_calc_test() ->
-  loadBalance:startServer(),
+  loadBalance:startServers(),
   ?assertEqual(0, loadBalance:numberOfRunningFunctions(1)),
   Ref = make_ref(),
   loadBalance:calcFun(self(), fun() -> func_to_calc(25, 1000) end, Ref),
@@ -23,17 +23,25 @@ simple_calc_test() ->
 
 
 load_balance_test() ->
-  loadBalance:startServer(),
-  NumOfFunction = 30,
-  Functions = [{make_ref(), fun() -> func_to_calc(Res, 1000) end, Res} || Res <- lists:seq(1, NumOfFunction)],
+  loadBalance:startServers(),
+
+  NumOfFunction = 10 * 3, % Should be divided equally by the 3 servers
+  WaitTime = 1000, % in ms
+
+  Functions = [{make_ref(), fun() -> func_to_calc(Res, WaitTime) end, Res} || Res <- lists:seq(1, NumOfFunction)],
   [loadBalance:calcFun(self(), Func, Ref) || {Ref, Func, _Res} <- Functions],
   % Check each server runs exactly one function
   RunningCounts = [loadBalance:numberOfRunningFunctions(ServerNum) || ServerNum <- lists:seq(1, 3)],
-  ?assertEqual([round(NumOfFunction / 3) || _Server <- lists:seq(1,3)], RunningCounts),
+  ?assertEqual([round(NumOfFunction / 3) || _Server <- lists:seq(1, 3)], RunningCounts),
   [receive
      {Ref, Res} -> ?assertEqual(ExpectedRes, Res)
    end || {Ref, _Func, ExpectedRes} <- Functions],
   loadBalance:stopServers().
 
-
-%todo Test kill one of the servers, make sure comes back alive
+kill_servers_test() ->
+  loadBalance:startServers(),
+  [exit(whereis(ServerName), kill) || ServerName <- [server1, server2, server3]],
+  timer:sleep(100),
+  RunningCount = [loadBalance:numberOfRunningFunctions(ServerNum) || ServerNum <- lists:seq(1, 3)],
+  ?assertEqual([0, 0, 0], RunningCount),
+  loadBalance:stopServers().
