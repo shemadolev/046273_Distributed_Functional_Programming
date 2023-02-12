8c5e2a685c8507c1dd1316cf479eb7a5521d0405
Dolev Shema <shemadolev@gmail.com>
2023-02-12 19:06:44 +0200

    merge HW3 files

--- src/HW3/loadBalance.erl
+++ src/HW3/loadBalance.erl
@@ -2,22 +2,34 @@
 -module(loadBalance).
 
 %% API
--export([startServer/0, stopServers/0, numberOfRunningFunctions/1, calcFun/3]).
+-export([startServers/0, stopServers/0, numberOfRunningFunctions/1, calcFun/3]).
+
+serverNumToAtom(ServerNumber) ->
+  list_to_atom(string:concat("server", integer_to_list(ServerNumber))).
 
 %% Starts all servers.
-startServer() ->
-  erlang:error(not_implemented).
+startServers() ->
+  servers_supervisor:start().
 
 %% Stops all servers.
 stopServers() ->
-  erlang:error(not_implemented).
+  servers_supervisor:stop_all().
 
 %% Gets the number of one of the servers; Returns the number of functions that server is running now.
-numberOfRunningFunctions(_ServerNumber) ->
-  erlang:error(not_implemented).
+numberOfRunningFunctions(ServerNumber) ->
+  ServerName = serverNumToAtom(ServerNumber),
+  fun_server:get_current_functions_count(ServerName).
 
 %% Gets the client's PID, a function, and some MsgRef; Returns ok, executes the function on the least busy server and
 %% when done sends a message back to the client's PID with {MsgRef, Result}, where Result is the returned value from the
 %% function.
-calcFun(_ClientPID, _ClientFun, _MsgRef) ->
-  erlang:error(not_implemented).
+calcFun(ClientPID, ClientFun, MsgRef) ->
+  % Get number of running functions for each server
+  AllRunning = [{serverNumToAtom(ServerNum), numberOfRunningFunctions(ServerNum)} || ServerNum <- lists:seq(1, 3)],
+  % Sort servers by their number of running functions, ascending order
+  SortedServers = lists:sort(fun({_ServName1, FunCount1}, {_ServName2, FunCount2}) -> FunCount1 < FunCount2 end, AllRunning),
+  % First server is the one with the least running functions - execute the function there
+  [{ChosenSever, _FunCount} | _Others] = SortedServers,
+  fun_server:calc_fun(ChosenSever, ClientPID, ClientFun, MsgRef),
+  ok.
+
